b6cdba15353abe44ff58826f3da59f152cbbe5f3
ShopLocket <ShopLocket@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-28 05:16:31 +0000

    Connect with ShopLocket; browse and embed your own products
    git-svn-id: https://plugins.svn.wordpress.org/shoplocket/trunk@688271 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- class-shoplocket-widget.php
+++ class-shoplocket-widget.php
@@ -21,7 +21,8 @@ class ShopLocket_Widget extends WP_Widget {
 	}
 
 	function update( $new_instance, $old_instance ) {
-		$instance = ShopLocket::normalize_args( $new_instance );
+		$instance = array();
+		$new_instance = ShopLocket::normalize_args( $new_instance );
 
 		if ( isset( $new_instance['code'] ) ) {
 			$code = trim( $new_instance['code'] );
@@ -38,16 +39,7 @@ class ShopLocket_Widget extends WP_Widget {
 
 		if ( isset( $new_instance['title'] ) )
 			$instance['title'] = sanitize_text_field( $new_instance['title'] );
-
-		if ( isset( $new_instance['width'] ) )
-			$instance['width'] = sanitize_text_field( $new_instance['width'] );
-
-		if ( isset( $new_instance['height'] ) )
-			$instance['height'] = sanitize_text_field( $new_instance['height'] );
-
-		// TODO: validate height/width to make sure they're sane values
-		// TODO: allow inserting shortcode into widget
-
+		
 		return $instance;
 	}
 
@@ -60,35 +52,23 @@ class ShopLocket_Widget extends WP_Widget {
 		// TODO: show error on invalid product id
 		?>
 
-		<?php if ( ! empty( $instance['url'] ) ) : ?>
-			<p>
-				<?php printf( __( 'Your selected product<br /> <a href="%s" target="_blank" title="Opens in new window">%s</a>', 'shoplocket' ), esc_url( $instance['url'] ), esc_html( $instance['url'] ) ); ?>
-			</p>
-		<?php endif; ?>
-		<p>
-			<label for="<?php echo $this->get_field_id( 'code' ); ?>">
-				<?php _e( 'Enter the embed, URL, or ID of your ShopLocket product', 'shoplocket' ); ?>
-			</label>
-			<br />
-			<textarea id="<?php echo $this->get_field_id( 'code' ); ?>" name="<?php echo $this->get_field_name( 'code' ); ?>" class="widefat"><?php echo esc_textarea( $instance['id'] ); ?></textarea>
-			<small><a href="<?php echo esc_url( ShopLocket::HELP_URL ); ?>" target="_blank" title="<?php esc_attr_e( 'Confused? Click through for instructions on finding your product emebed code.', 'shoplocket' ); ?>"><?php _e( 'What can you find this?', 'shoplocket' ); ?></a></small>
-		</p>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'title' ); ?>">
-				<?php esc_html_e( 'Title', 'shoplocket' ); ?>
+				<?php esc_html_e( 'Title:', 'shoplocket' ); ?>
 				<input id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $instance['title'] ); ?>" class="widefat" />
 			</label>
 		</p>
 		<p>
-			<label for="<?php echo $this->get_field_id( 'width' ); ?>">
-				<?php esc_html_e( 'Width', 'shoplocket' ); ?>
-				<input size="3" id="<?php echo $this->get_field_id( 'width' ); ?>" name="<?php echo $this->get_field_name( 'width' ); ?>" type="text" value="<?php echo esc_attr( $instance['width'] ); ?>" />
+			<label for="<?php echo $this->get_field_id( 'code' ); ?>">
+				<?php _e( 'Product:' ); ?>
 			</label>
-
-			<label for="<?php echo $this->get_field_id( 'height' ); ?>">
-				<?php esc_html_e( 'Height', 'shoplocket' ); ?>
-				<input size="3" id="<?php echo $this->get_field_id( 'height' ); ?>" name="<?php echo $this->get_field_name( 'height' ); ?>" type="text" value="<?php echo esc_attr( $instance['height'] ); ?>" />
-			</label>
+			
+			<!-- TODO: Add Refresh Button -->
+			
+			<select name="<?php echo $this->get_field_name( 'code' ); ?>">
+				<option data-url="<?php ShopLocket::get_product_url_from_id( $instance['id'] ); ?>" value="">&#8212; Select &#8212;</option>
+				<?php echo ShopLocket::shoplocket_get_options_for_product_list(get_option("shoplocket_products_json"),$instance['id']); ?>  
+			</select>
 		</p>
 		<?php
 	}
